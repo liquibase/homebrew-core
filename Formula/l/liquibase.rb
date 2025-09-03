c6f6009cff5e32f8d04b46bf205537abfba83115
Sayali Mohadikar <smohadikar@liquibase.com>
2025-09-03 11:15:37 -0500

    Update Liquibase URL

--- Formula/l/liquibase.rb
+++ Formula/l/liquibase.rb
@@ -1,7 +1,7 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/v4.32.0/liquibase-4.32.0.tar.gz"
+  url "https://package.liquibase.com/downloads/liquibase/homebrew/liquibase-4.33.0.tar.gz"
   sha256 "10910d42ae9990c95a4ac8f0a3665a24bd40d08fb264055d78b923a512774d54"
   license "Apache-2.0"
 
